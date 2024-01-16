6f3ff16cc3ed922bfcce02e29d4f30c77a9e53b9
cmanv <cmangin@arobas.net>
2024-01-16 11:39:08 -0500

    Pass desktops parameters (first, last, offset)

--- statusbar.sh
+++ statusbar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export TCLLIBPATH=$HOME/.local/lib
 export LD_PRELOAD=/usr/local/lib/libX11.so:/usr/local/lib/libmpdclient.so
-exec ~/.local/scripts/statusbar.tk
+exec ~/.local/scripts/statusbar.tk $*
