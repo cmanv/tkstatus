12a5310dc0b16267db19d8dd2c06316ae71de837
cmanv <cmanv@duck.com>
2025-08-28 12:12:39 -0400

    zconf -> zconfig

---
+++ modules/zconfig/zconfig.tcl
@@ -0,0 +1,109 @@
+#!/usr/bin/env tclsh9.0
+package require utils
+namespace eval zconfig {
+	variable defaultfile "$::env(HOME)/.config/zstatus/config"
+
+	array set config [ list \
+		fontname	NotoSans\
+		fontsize	11\
+		geometry	"1600x26+0+0i"\
+		emojifontname	NotoSansEmoji\
+		emojifontsize	11\
+		xscreen		0\
+		socket		"$::env(XDG_CACHE_HOME)/zstatus/socket"\
+		wmsocket 	"$::env(XDG_CACHE_HOME)/zwm/socket"\
+		themefile 	"$::env(XDG_STATE_HOME)/theme/current"]
+
+	namespace export read general
+}
+
+proc zconfig::general {configfile} {
+	variable defaultfile
+	variable config
+
+	if {$configfile == {default}} {
+		set configfile $defaultfile
+	}
+
+	if [file exists $configfile] {
+		set section ""
+		set lines [utils::read_file $configfile]
+		foreach line $lines {
+			if ![string length $line] { continue }
+			if [regexp {^#} $line] { continue }
+			if [regexp {^\[([a-z_]+)\]} $line -> section] {
+				if {$section != "general"} {
+					set section ""
+					continue
+				}
+			}
+			if ![string length $section] { continue }
+			process_general $line
+		}
+	}
+
+	return [array get config]
+}
+
+proc zconfig::read {configfile} {
+	variable defaultfile
+	variable config
+	variable lwidgets {}
+	variable rwidgets {}
+
+	set sections {general left_widgets right_widgets}
+	set default_lwidgets {deskmode separator desklist separator\
+			deskname separator wintitle}
+	set default_rwidgets {datetime}
+
+	if {$configfile == {default}} {
+		set configfile $defaultfile
+	}
+
+	if [file exists $configfile] {
+		set section ""
+		set lines [utils::read_file $configfile]
+		foreach line $lines {
+			if ![string length $line] { continue }
+			if [regexp {^#} $line] { continue }
+			if [regexp {^\[([a-z_]+)\]} $line -> section] {
+				if {[lsearch $sections $section] < 0} {
+					set section ""
+				}
+				continue
+			}
+			if ![string length $section] { continue }
+			process_$section $line
+		}
+	}
+
+	if ![llength $lwidgets] {
+		set lwidgets $default_lwidgets
+	}
+	if ![llength $rwidgets] {
+		set rwidgets $default_rwidgets
+	}
+	set config(lwidgets) $lwidgets
+	set config(rwidgets) $rwidgets
+
+	return [array get config]
+}
+
+proc zconfig::process_general {line} {
+	variable config
+	if [regexp {^([^=]+)=(.+)} $line -> key value] {
+		set config($key) $value
+	}
+}
+
+proc zconfig::process_left_widgets {line} {
+	variable lwidgets
+	lappend lwidgets $line
+}
+
+proc zconfig::process_right_widgets {line} {
+	variable rwidgets
+	lappend rwidgets $line
+}
+
+package provide zconfig 0.1
